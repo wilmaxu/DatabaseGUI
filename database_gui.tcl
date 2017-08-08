536d72c4d9a0d7f965398b198591f6193292e7c5
Yiwen Xu <wilma.xyw@gmail.com>
2017-08-08 14:28:33 -0400

    Add files via upload

--- database_gui.tcl
+++ database_gui.tcl
@@ -15,7 +15,7 @@ grid [label .db.glbl -text "g"] -column 1 -row 8 -pady 5 -sticky w
 grid [label .db.nlbl -text "n"] -column 1 -row 9 -pady 5 -sticky w
 grid [label .db.rlbl -text "Reference"] -column 1 -row 10 -pady 5 -sticky w
 
-grid [entry .db.waveval -textvariable db_wavelength ] -column 2 -row 4 -pady 5 -sticky w
+grid [entry .db.waveval -textvariable db_wavelength ] -column 2 -row 4 -pady 5 -sticky we
 grid [ttk::combobox .db.organval -values [list air bladder blood bone brain breast CSF fat gray_matter heart liver lung muscle scalp&skull skin water white_matter] -textvariable db_organ] -column 2 -row 1 -pady 5 -sticky w
 grid [ttk::combobox .db.sttval -values [list healthy tumor none] -textvariable db_status] -column 2 -row 2 -pady 5 -sticky w
 grid [ttk::combobox .db.speciesval -values [list human rat rabbit pig dog none] -textvariable db_species] -column 2 -row 3 -pady 5 -sticky w
@@ -27,6 +27,6 @@ grid [entry .db.g -textvariable db_g ] -column 2 -row 8 -pady 5 -sticky we
 grid [entry .db.n -textvariable db_n ] -column 2 -row 9 -pady 5 -sticky we
 grid [entry .db.r -textvariable db_r ] -column 2 -row 10 -pady 5 -sticky we
 
-grid [button .db.addopt -text "Add to Database" -command AddToDatabase] -column 3 -row 9 -pady 5 -sticky we
+grid [button .db.addopt -text "Add to Database" -command AddToDatabase] -column 2 -row 11 -pady 5 -sticky we
 
 
